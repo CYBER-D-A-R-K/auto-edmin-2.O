cdc460f9b09390d4e8424999f39dfced655c928b
ᴄyʙᴇʀ_ᴅᴀʀᴋ <89356302+CYBER-D-A-R-K@users.noreply.github.com>
2022-06-20 15:05:00 +0530

    Create edmin.py

---
+++ edmin.py
@@ -0,0 +1,83 @@
+import os
+import sys
+os.system('pip install colorama')
+
+from colorama import Fore
+
+def SlowPrint(txt):
+       for x in txt:
+              print(x, flush=True)
+              print()
+
+red = Fore.RED
+cyan = Fore.CYAN
+blue = Fore.BLUE
+green = Fore.GREEN
+green2 = Fore.LIGHTGREEN_EX
+white = Fore.WHITE
+os.system ("clear")
+import time
+from time import sleep
+
+
+a=25
+b=2
+while b<a:
+  print("\033[1;31m\nInstaling................!!")
+  sleep (0.10)
+  b += 1
+print()
+print()
+print()
+
+import time
+from time import sleep
+
+
+a=10
+b=1
+while b<a:
+     print("\033[3;31;40m\nPlease Wait...............!!")
+     sleep (0.3)
+     b += 1
+print()
+print( "\033[1;31m\nInstaling successfull..........!!" )
+print()
+print()
+print()
+print("\033[0;31;40m\n !......▆▆         ▆▆.....!")
+print("\033[0;31;40m\n !......▆▆         ▆▆.....!")
+print("\033[0;31;40m\n !......▆▆▆▆▆▆▆▆▆▆▆▆.....!")
+print("\033[0;31;40m\n !......▆▆         ▆▆.....!")
+print("\033[0;31;40m\n !......▆▆         ▆▆.....!")
+print()
+print()
+os.system("clear")
+os.system("figlet edmin Hack | lolcat")
+print (f"                 { blue} coded by cyber dark")
+print()
+x = input (f"{red} Enter edmin hack group link : " )
+
+import time
+from time import sleep
+x=100
+y=3
+while y<x:
+    print("\033[1;31m\n𝚂𝚎𝚊𝚛𝚌𝚑𝚒𝚗𝚐.........................................!!!!")
+    sleep (0.5)
+    y +=10
+print()
+print()
+print("𝙲𝚘𝚖𝚙𝚕𝚒𝚝𝚎𝚍.....")
+print()
+
+y = input (f"{red} Enter Your number : " )
+
+x=100
+y=5
+while x>y :
+        print("\033[1;31mTrying to HEMKE............!")
+        sleep(1)
+        y += 5
+
+os.system("figlet ayyo hemke ayi | lolcat")
